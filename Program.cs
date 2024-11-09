054fe31f3828df96f3f793bf9a121ee5a55116f8
Edeson Junior <edeson.jsbarbosa@hotmail.com>
2024-11-09 11:39:59 -0300

    BUG_TODO corrigido  - Melhorias

--- Program.cs
+++ Program.cs
@@ -1,23 +1,48 @@
 ﻿using System.Text;
 using DesafioProjetoHospedagem.Models;
+using Microsoft.VisualBasic;
+using System.Globalization;
 
 Console.OutputEncoding = Encoding.UTF8;
 
+ //CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
-// Pessoa p3 = new Pessoa(nome: "Hóspede 3");
+System.Console.WriteLine("Quantas pessoas irão hospedar?");
+int quantidadeHospesdes = int.TryParse(Console.ReadLine(), out var quantidade) ? quantidade : 1;
 
-hospedes.Add(p1);
-hospedes.Add(p2);
-// hospedes.Add(p3);
+for(int i = 0; i < quantidadeHospesdes; i++)
+{
+    Pessoa hospede = new Pessoa();
+    System.Console.WriteLine($"Digite o Nome do hospede {i + 1}:");
+    string nome = Console.ReadLine() ?? "";
+    hospede.Nome = nome;
+    System.Console.WriteLine($"Digite o Sobrenome do hospede {i + 1}:");
+    string sobrenome = Console.ReadLine() ?? "";
+    hospede.Sobrenome = sobrenome;
+
+    hospedes.Add(hospede);
+}
 
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
-Reserva reserva = new Reserva(diasReservados: 11);
+System.Console.WriteLine("Quantos dias irão hospedar?");
+int quantidadeDias = int.TryParse(Console.ReadLine(), out var dias) ? dias : 1;
+
+Reserva reserva = new Reserva(diasReservados: quantidadeDias);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+System.Console.WriteLine("\n ---- Detalhes da reserva ---------");
+
+Console.WriteLine($"Total de Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor total da reserva para {quantidadeDias} dias: {reserva.CalcularValorDiaria().ToString("C", CultureInfo.GetCultureInfo("pt-BR"))}");
+
+
+System.Console.WriteLine("\n ---- Lista de Hospedes ---------");
+
+for(int i = 0; i < hospedes.Count; i++)
+{
+    hospedes[i].ListarHospedes(i + 1);
+}
